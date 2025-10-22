f7e12ad6f8aace3a405ed560468b1deb0cc842dc
William-Almeida <williamkillerca@hotmail.com>
2025-10-21 22:27:28 -0300

    fix: include id in UUID generation for Dialog component

--- src/View/Components/Dialog.php
+++ src/View/Components/Dialog.php
@@ -11,11 +11,12 @@ class Dialog extends Component
     public string $uuid;
 
     public function __construct(
+        public ?string $id = null,
         public ?string $position = 'center',
         public ?bool $showBackdrop = true,
         public ?bool $blurBackdrop = false
     ) {
-        $this->uuid = "mary" . md5(serialize($this));
+        $this->uuid = "mary" . md5(serialize($this)) . $id;
     }
 
     public function render(): View|Closure|string
